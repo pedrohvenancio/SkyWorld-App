4487d8c39ee16bbbe1eaaa452b34366a4b54cb65
Pedro Venâncio <ph.venancio2@gmail.com>
2023-01-14 22:47:36 -0300

    upgrades

--- screens/LoginInUp.js
+++ screens/LoginInUp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '../styles/LoginInUpStyle';
 import { StyleSheet, View } from 'react-native';
-import { Button, ButtonGroup, withTheme, Text } from '@rneui/themed';
+import { Button, ButtonGroup, withTheme, LinearGradient, Text } from '@rneui/themed';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 
@@ -13,13 +13,24 @@ function LoginInUp() {
 
     return (
     <View style={styles.container}>
-        <Text>Oi</Text>
-        <Button radius={'lg'} type="solid" onPress={() => nav.navigate('LoginIn')}>
-            Login
-        </Button>
-        <Button radius={'lg'} type="solid" onPress={() => nav.navigate('LoginUp')}>
-            registrar
-        </Button>
+        <View style={styles.view1}>
+        </View>
+        <View style={styles.view2}>
+            <Text>Oi</Text>
+            <Button ViewComponent={LinearGradient} // Don't forget this!
+                linearGradientProps={{
+                    colors: ['#FF9800', '#F44336'],
+                    start: { x: 0, y: 0.5 },
+                    end: { x: 1, y: 0.5 },
+                }} 
+                onPress={() => nav.navigate('LoginIn')}>
+                Login
+            </Button>
+            <Button radius={'lg'} type="solid" onPress={() => nav.navigate('LoginUp')}>
+                Registrar
+            </Button>
+        </View>
+        
     </View>
     );
 }
--- styles/LoginInUpStyle.js
+++ styles/LoginInUpStyle.js
@@ -4,10 +4,23 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      padding: 0,
+      margin: 0,
     },
-    
+    view1: {
+      flex: 2,
+      padding: 10,
+      maxHeight: '60%',
+    },
+    logo: {
+      width: 100,
+      height: 100,
+    },
+    view2: {
+      flex: 1,
+      padding: 10,
+      maxHeight: '40%',
+    },
   });
 
 export default styles
